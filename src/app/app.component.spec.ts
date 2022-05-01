b131fe46f74137e061579e79d733f147e0bf4243
Jonathan002 <jonathanf002@gmail.com>
2022-05-01 15:53:48 -0700

    stabilized test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -2,31 +2,34 @@ import { MockBuilder, MockedComponentFixture, MockRender } from 'ng-mocks';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
-  let fixture: MockedComponentFixture
+  let fixture: MockedComponentFixture<AppComponent>
   let params: { waterState: string };
   beforeEach(async () => {
     console.log('===============');
     await MockBuilder(AppComponent)
-    const params = { waterState: 'liquid' };
-    fixture = MockRender(AppComponent, params)
+    params = { waterState: 'liquid' };
+    fixture = MockRender(AppComponent, params as any) // Typed as any as it requires all properties and methods instead of just @Input()..
   });
 
-  it(`should trigger ngOnChanges`, () => {
-    console.log('fixture.point.componentInstance.waterState');
+  it(`should trigger ngOnChanges normally - with fixture.point.componentInstance`, () => {
+    console.log('--- updating fixture.point.componentInstance.waterState ---');
     fixture.point.componentInstance.waterState = 'frozen';
     fixture.detectChanges() // frozen wont trigger
+    expect(fixture.point.componentInstance.ngOnChangesCalls).toBe(2)
   });
 
   it(`should trigger ngOnChanges normally - with params`, () => {
-    console.log('params.waterState');
+    console.log('--- updating params.waterState ----');
     params.waterState = 'gas';
-    fixture.detectChanges() // gas wont trigger
+    fixture.detectChanges() // will trigger but fixture.point.componentInstance is left behind..
+    expect(fixture.point.componentInstance.ngOnChangesCalls).toBe(2)
   });
 
-  it(`should trigger ngOnChanges normally`, () => {
-    console.log('fixture.componentInstance.waterState');
+  it(`should trigger ngOnChanges normally - with fixture.componentInstance`, () => {
+    console.log('-- updating fixture.componentInstance.waterState ---');
     (fixture.componentInstance as any).waterState = 'fourthUnknownState';
     fixture.detectChanges()
+    expect(fixture.point.componentInstance.ngOnChangesCalls).toBe(2)
   });
 
 
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,17 @@
-import { Component, Input, SimpleChanges } from '@angular/core';
+import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnChanges {
 
   @Input() waterState: string = 'liquid';
-  title = 'basic-angular';
-
+  ngOnChangesCalls = 0;
   ngOnChanges(changes: SimpleChanges) {
     console.log('log changes.current', changes['waterState'].currentValue);
     console.log('log changes.previous', changes['waterState'].previousValue);
+    this.ngOnChangesCalls++
   }
 }
