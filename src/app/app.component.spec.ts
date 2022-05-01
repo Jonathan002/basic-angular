474b7b9f4d347fac9b1b7c8efd2f92c0150ea036
Jonathan002 <jonathanf002@gmail.com>
2022-05-01 15:56:00 -0700

    made things simpler

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -4,6 +4,7 @@ import { AppComponent } from './app.component';
 describe('AppComponent', () => {
   let fixture: MockedComponentFixture<AppComponent>
   let params: { waterState: string };
+  const updatedState = 'frozen';
   beforeEach(async () => {
     console.log('===============');
     await MockBuilder(AppComponent)
@@ -13,23 +14,26 @@ describe('AppComponent', () => {
 
   it(`should trigger ngOnChanges normally - with fixture.point.componentInstance`, () => {
     console.log('--- updating fixture.point.componentInstance.waterState ---');
-    fixture.point.componentInstance.waterState = 'frozen';
+    fixture.point.componentInstance.waterState = updatedState;
     fixture.detectChanges() // frozen wont trigger
     expect(fixture.point.componentInstance.ngOnChangesCalls).toBe(2)
+    expect(fixture.point.componentInstance.waterState).toBe(updatedState)
   });
 
   it(`should trigger ngOnChanges normally - with params`, () => {
     console.log('--- updating params.waterState ----');
-    params.waterState = 'gas';
+    params.waterState = updatedState;
     fixture.detectChanges() // will trigger but fixture.point.componentInstance is left behind..
     expect(fixture.point.componentInstance.ngOnChangesCalls).toBe(2)
+    expect(fixture.point.componentInstance.waterState).toBe(updatedState)
   });
 
   it(`should trigger ngOnChanges normally - with fixture.componentInstance`, () => {
     console.log('-- updating fixture.componentInstance.waterState ---');
-    (fixture.componentInstance as any).waterState = 'fourthUnknownState';
+    (fixture.componentInstance as any).waterState = updatedState;
     fixture.detectChanges()
     expect(fixture.point.componentInstance.ngOnChangesCalls).toBe(2)
+    expect(fixture.point.componentInstance.waterState).toBe(updatedState)
   });
 
 
